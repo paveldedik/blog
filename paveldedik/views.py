60f387b66098985d39f2c4a95c7eeb54c167a4b4
Pavel Dedik <dedikx@gmail.com>
2013-08-09 20:48:52 +0200

    Added a new endpoint for creation of a new post.

--- paveldedik/views.py
+++ paveldedik/views.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from paveldedik import app
-from paveldedik.models import Post
+from paveldedik.utils import slugify
+from paveldedik.models import Post, PostForm
 
 
 @app.route('/')
+@app.route('/blog')
+@app.route('/posts')
 def index():
     """Homepage of the application.
 
@@ -19,5 +22,27 @@ def index():
 
 @app.route('/posts/<string:post_id>')
 def show_post(post_id):
+    """Endpoint for posts. See :class:`~paveldedik.models.Post`.
+
+    :param post_id: ID of the post.
+    :type post_id: string
+    :return: HTML document.
+    """
     post = Post.objects.get_or_404(post_id=post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+def add_post():
+    """Displays form for creation of a new post. If the form is submitted
+    the post is saved in the database.
+
+    :return: HTML document.
+    """
+    form = PostForm(request.form)
+    if form.validate_on_submit():
+        post_id = slugify(form.title.data)
+        Post(title=form.title.data, leading=form.leading.data,
+             content=form.content.data, post_id=post_id).save()
+        return redirect(url_for('show_post', post_id=post_id))
+    return render_template('new_post.html', form=form)
