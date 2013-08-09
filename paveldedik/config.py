e29d37329c95700239bca35b4037b2a703917f71
Pavel Dedik <dedikx@gmail.com>
2013-08-09 20:46:31 +0200

    Documentation.

--- paveldedik/config.py
+++ paveldedik/config.py
@@ -4,6 +4,11 @@
 from paveldedik import app
 
 
+#: Debug mode enabled by default.
 DEBUG = True
 
+#: Secret key set in order to use sessions and CSRF validation.
+SECRET_KEY = 'development key'
+
+#: Mongo database settings.
 MONGODB_SETTINGS = {'DB': app.name}
