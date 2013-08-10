2b035306c9d7a21f693c4ad7e2ff6660443f5c25
Pavel Dedik <dedikx@gmail.com>
2013-08-10 23:00:57 +0200

    Relocated jinja2 html filter setting.

--- paveldedik/__init__.py
+++ paveldedik/__init__.py
@@ -4,13 +4,9 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
-from paveldedik.utils import to_html
-
 
 app = Flask(__name__)
 app.config.from_object('paveldedik.config')
-
-app.jinja_env.filters['html'] = to_html
 
 db = MongoEngine(app)
 
