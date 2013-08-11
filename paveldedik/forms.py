cddb0ae5c9c2d96c5902943f8b341ab2b698235f
Pavel Dedik <dedikx@gmail.com>
2013-08-11 15:37:01 +0200

    Exclude post_id from the wtform.

--- paveldedik/forms.py
+++ paveldedik/forms.py
@@ -6,13 +6,12 @@ from flask.ext.mongoengine.wtf import model_form
 from paveldedik.models import User, Post
 
 
-post_args = {
-    'title': {'label': u'Title'},
-    'leading': {'label': u'Leading'},
-    'content': {'label': u'Content'},
-}
-
-
+#: Model the user form. Additional field arguments can be included using
+#: the key-word argument ``field_args``. For more information about using
+#: WTForms follow `this link<http://flask.pocoo.org/snippets/60/>`_.
 UserForm = model_form(User)
 
-PostForm = model_form(Post, field_args=post_args)
+#: Model the post form. The attribute ``post_is`` must be excluded so that
+#: the field is not required during form validation and it is not rewritten
+#: when calling `populate_obj` on the :class:`models.Post` instance.
+PostForm = model_form(Post, exclude=['post_id'])
