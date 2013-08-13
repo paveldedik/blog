6df0181a35d8dba3d087b7da330e7950897a97b6
Pavel Dedik <dedikx@gmail.com>
2013-08-13 18:35:58 +0200

    Import filters in views module.

--- paveldedik/views.py
+++ paveldedik/views.py
@@ -3,7 +3,7 @@
 
 from flask import render_template, request, redirect, url_for
 
-from paveldedik import app
+from paveldedik import app, filters
 from paveldedik.utils import slugify
 from paveldedik.forms import PostForm
 from paveldedik.models import Post
